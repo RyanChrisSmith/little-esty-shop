dc7ddc420b41336c34b00dbc91581df25a15ecc7
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-23 14:13:05 -0600

    fix

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -15,14 +15,14 @@ class BulkDiscountsController < ApplicationController
   def create
     merchant = Merchant.find(params[:merchant_id])
     discount = merchant.bulk_discounts.create(discount_params)
+    
     redirect_to merchant_bulk_discounts_path(merchant)
   end
 
   def destroy 
     merchant = Merchant.find(params[:merchant_id])
     bulk_discount = BulkDiscount.find(params[:id])
-    bulk_discount.destroy
-    
+    bulk_discount.destroy    
     redirect_to merchant_bulk_discounts_path(merchant)
   end 
 
