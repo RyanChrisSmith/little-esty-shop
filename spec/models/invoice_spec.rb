a211abbda4e3ba1b6d509bdd1dc609ef1de61ddb
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-24 11:15:02 -0600

    all model tests and scenarios for discounted revenue method passing

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -78,13 +78,35 @@ RSpec.describe Invoice, type: :model do
       @invoice_item_1 = InvoiceItem.create!(quantity: 12, unit_price: 100, status: 2, item_id: @item_2.id, invoice_id: @invoice_1.id)
       @invoice_item_2 = InvoiceItem.create!(quantity: 15, unit_price: 100, status: 1, item_id: @item_4.id, invoice_id: @invoice_1.id)
       #12 * 100 = 1200 * .2 = 240 - item A discount
-      #15 * 100 = 1500 * .15 = 225 - item B discount - should be ignored
       #15 * 100 = 1500 * .2 = 300 - item B discount
+      #15 * 100 = 1500 * .15 = 225 - item B discount - should be ignored
       @discount_a = BulkDiscount.create!(percentage_discount: 20, quantity: 10, merchant_id: @merchant_1.id)
       @discount_b = BulkDiscount.create!(percentage_discount: 15, quantity: 15, merchant_id: @merchant_1.id)
 
       expect(@invoice_1.discounted_revenue).to eq 540
       expect(@invoice_1.discounted_revenue).to_not eq 465
+    end
+
+    it 'displays total discount where both item A should be 20% and item B 30% for merchant A, but the included item A for merchant B is not discounted' do
+      @merchant_1 = Merchant.create!(name: "Bread Pitt")
+      @merchant_2 = Merchant.create!(name: "Carrie Breadshaw")
+      @item_1 = Item.create!(name: "Sourdough", description: "leavened, wild yeast", unit_price: 400, merchant_id: @merchant_1.id)
+      @item_2 = Item.create!(name: "Baguette", description: "Soft, french", unit_price: 100, merchant_id: @merchant_1.id)
+      @item_4 = Item.create!(name: "Bread Roll", description: "Round, soft", unit_price: 100, merchant_id: @merchant_2.id, status: 1)
+      @customer_1 = Customer.create!(first_name: "Meat", last_name: "Loaf")
+      @invoice_1 = Invoice.create!(status: 2, customer_id: @customer_1.id, created_at: Time.parse("Friday, September, 16, 2022"))
+      @invoice_item_1 = InvoiceItem.create!(quantity: 12, unit_price: 100, status: 2, item_id: @item_1.id, invoice_id: @invoice_1.id)
+      @invoice_item_2 = InvoiceItem.create!(quantity: 15, unit_price: 100, status: 1, item_id: @item_2.id, invoice_id: @invoice_1.id)
+      @invoice_item_3 = InvoiceItem.create!(quantity: 15, unit_price: 100, status: 2, item_id: @item_4.id, invoice_id: @invoice_1.id)
+      #12 * 100 = 1200 * .2 = 240 - item A discount
+      #15 * 100 = 1500 * .3 = 450 - item B discount
+      #merchant_2 does not have disounts
+      #15 * 100 = 1500 * .2 = 300 - item A discount - should be ignored
+      @discount_a = BulkDiscount.create!(percentage_discount: 20, quantity: 10, merchant_id: @merchant_1.id)
+      @discount_b = BulkDiscount.create!(percentage_discount: 30, quantity: 15, merchant_id: @merchant_1.id)
+
+      expect(@invoice_1.discounted_revenue).to eq 690
+      expect(@invoice_1.discounted_revenue).to_not eq 990
     end
 
   end
