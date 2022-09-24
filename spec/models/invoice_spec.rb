5e8397fa6478238e2ce223dbb84773387b288368
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-24 10:50:57 -0600

    second scenario passing

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -39,6 +39,20 @@ RSpec.describe Invoice, type: :model do
       expect(@invoice_1.discounted_revenue).to eq 0
     end
 
+    it 'shows the discount when item A is discounted for 20% and item B is not discounted' do
+      @merchant_1 = Merchant.create!(name: "Bread Pitt")
+      @item_2 = Item.create!(name: "Baguette", description: "Soft, french", unit_price: 100, merchant_id: @merchant_1.id)
+      @item_4 = Item.create!(name: "Bread Roll", description: "Round, soft", unit_price: 100, merchant_id: @merchant_1.id, status: 1)
+      @customer_1 = Customer.create!(first_name: "Meat", last_name: "Loaf")
+      @invoice_1 = Invoice.create!(status: 2, customer_id: @customer_1.id, created_at: Time.parse("Friday, September, 16, 2022"))
+      @invoice_item_1 = InvoiceItem.create!(quantity: 10, unit_price: 100, status: 2, item_id: @item_2.id, invoice_id: @invoice_1.id)
+      @invoice_item_2 = InvoiceItem.create!(quantity: 5, unit_price: 100, status: 1, item_id: @item_4.id, invoice_id: @invoice_1.id)
+
+      @discount = BulkDiscount.create!(percentage_discount: 20, quantity: 10, merchant_id: @merchant_1.id)
+
+      expect(@invoice_1.discounted_revenue).to eq 200
+    end
+
   end
 
 
